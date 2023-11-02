ec3773f1c19c55cea2040b43cd4e83e6db538c3a
DESKTOP-SEEUN\tpdms <grace7040@naver.com>
2023-11-03 06:13:32 +0900

    Rename: 스프라이트 폴더 정리

---
+++ Assets/Scripts/Obastacles/ColorChargingStation.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChargingStation : MonoBehaviour
+{
+    public Colors paintColor;
+    public GameObject[] otherPaintObjects;
+    private static object _lock = new object();
+    static bool isSelected = false;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            if (isSelected)
+                return;
+
+            lock (_lock)
+            {
+                isSelected = true;
+                foreach (GameObject paint in otherPaintObjects)
+                    paint.SetActive(false);
+            }
+               
+            GetComponent<BoxCollider2D>().isTrigger = true;
+
+            switch (paintColor)
+            {
+                case Colors.red:
+                    ColorManager.Instance.hasRed = true;
+                    break;
+                case Colors.blue:
+                    ColorManager.Instance.hasBlue = true;
+                    break;
+                case Colors.yellow:
+                    ColorManager.Instance.hasYellow = true;
+                    break;
+            }
+
+            this.CallOnDelay(3f, ()=> { gameObject.SetActive(false); isSelected = false; });
+        }
+    }
+}
