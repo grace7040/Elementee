5c1f2015aa75d261dce2c7311c2e80c259557a82
Heokunho <93777460+Heokunho@users.noreply.github.com>
2023-11-17 10:32:11 +0900

    Fix: SlipperyGround

--- Assets/Scripts/Obastacles/OB_SlipperyGround.cs
+++ Assets/Scripts/Obastacles/OB_SlipperyGround.cs
@@ -4,34 +4,43 @@ using UnityEngine;
 
 public class OB_SlipperyGround : MonoBehaviour
 {
-    public float delayTime = 2f; // 사라지기까지 대기 시간
-    private bool playerOnPlatform = false;
+    private PhysicMaterial slipperyMaterial; // 마찰력을 없앨 PhysicMaterial
+    private Collider playerCollider; // 플레이어의 Collider
 
-    private void OnCollisionEnter(Collision collision)
+    private void Start()
     {
-        // 플레이어와 발판의 충돌을 확인합니다.
-        if (collision.gameObject.CompareTag("Player"))
+        // 마찰력을 없앨 PhysicMaterial 생성
+        slipperyMaterial = new PhysicMaterial();
+        slipperyMaterial.frictionCombine = PhysicMaterialCombine.Minimum;
+        slipperyMaterial.staticFriction = 0f;
+        slipperyMaterial.dynamicFriction = 0f;
+
+        // 플레이어의 Collider 가져오기
+        playerCollider = GameObject.FindGameObjectWithTag("Player").GetComponent<Collider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // 만약 충돌한 오브젝트가 "Player" 태그를 가진 플레이어라면
+        if (other.CompareTag("Player"))
         {
-            playerOnPlatform = true;
-            Invoke("DisappearPlatform", delayTime);
+            // 플레이어의 Collider에 마찰력을 없앤 PhysicMaterial 적용
+            if (playerCollider != null)
+            {
+                playerCollider.material = slipperyMaterial;
+            }
         }
     }
 
-    private void OnCollisionExit(Collision collision)
+    private void OnTriggerExit(Collider other)
     {
-        // 플레이어와 발판 사이의 충돌이 끝났음을 확인합니다.
-        if (collision.gameObject.CompareTag("Player"))
+        // 플레이어가 오브젝트를 빠져나갔을 때, 기존 PhysicMaterial로 복원
+        if (other.CompareTag("Player"))
         {
-            playerOnPlatform = false;
-        }
-    }
-
-    private void DisappearPlatform()
-    {
-        // 플레이어가 발판 위에 있을 때만 발판을 제거합니다.
-        if (playerOnPlatform)
-        {
-            Destroy(gameObject);
+            if (playerCollider != null)
+            {
+                playerCollider.material = null; // 기존 PhysicMaterial로 변경하거나 다른 PhysicMaterial로 변경
+            }
         }
     }
 }
