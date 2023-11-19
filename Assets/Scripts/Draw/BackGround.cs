42f391c1de8b541d75218b34da9fe519e61196a9
CHERISH61111 <84861196+CHERISH61111@users.noreply.github.com>
2023-11-20 02:13:07 +0900

    Feat: 세로로 그리기 / WeaponCanvas 크기 수정

--- Assets/Scripts/Draw/BackGround.cs
+++ Assets/Scripts/Draw/BackGround.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class BackGround : MonoBehaviour
 {
     public SpriteRenderer sr;
+    public float width = 0.25f;
+    public float height = 0.33333f;
+
     private void Start()
     {
-        GetComponent<Transform>().localScale = new Vector3(sr.sprite.rect.width*0.25f, sr.sprite.rect.height*0.33333f, 0);
+        GetComponent<Transform>().localScale = new Vector3(sr.sprite.rect.width* width, sr.sprite.rect.height* height, 0);
     }
 }
