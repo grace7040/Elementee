eb1feae9b981ce47dcf7e522e59e968cbca7d775
Heokunho <93777460+Heokunho@users.noreply.github.com>
2023-11-22 05:51:36 +0900

    Fix: Blue monster

--- Assets/Scripts/Color/MonsterColor/M_BlueColor.cs
+++ Assets/Scripts/Color/MonsterColor/M_BlueColor.cs
@@ -8,10 +8,14 @@ public class M_BlueColor : MonoBehaviour, M_IColorState
 
     public int M_damage { get { return 10; } }
 
+    GameObject Waters;
+
     public void Attack(MonsterController monster)
     {
         monster.GetComponent<Animator>().SetBool("IsAttacking", true);
         monster.CallOnDelay(1f, () => { GameObject Water = Instantiate(Resources.Load("Waters"), monster.transform.position, Quaternion.identity) as GameObject; });
+        monster.CallOnDelay(1f, () => { Waters = Instantiate(Resources.Load("Blue_Attack_Effect_"), monster.transform.position, Quaternion.Euler(0, 0, 0)) as GameObject; });
+        Destroy(Waters);
         
         //Debug.Log("Blue");
     }
