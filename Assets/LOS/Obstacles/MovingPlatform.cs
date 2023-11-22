79bb34b125f1b54ccc9468af802b6ac122e563cc
Heokunho <93777460+Heokunho@users.noreply.github.com>
2023-11-22 09:12:57 +0900

    Fix: MovingPlatform

--- Assets/LOS/Obstacles/MovingPlatform.cs
+++ Assets/LOS/Obstacles/MovingPlatform.cs
@@ -7,6 +7,7 @@ public class MovingPlatform : MonoBehaviour
     public Transform pivotPoint; // 발판의 중점
     public float moveRange = 5.0f; // 발판의 상하 운동 범위
     public float moveSpeed = 2.0f; // 이동 속도
+    public bool Vertical = true;
 
     private Vector3 originalPosition; // 발판의 초기 위치
 
@@ -17,8 +18,16 @@ public class MovingPlatform : MonoBehaviour
 
     void Update()
     {
-        float newY = Mathf.Sin(Time.time * moveSpeed) * moveRange; // Sin 함수를 사용하여 상하 운동 계산
-        transform.position = new Vector3(originalPosition.x, pivotPoint.position.y + newY, originalPosition.z);
+        if (Vertical)
+        {
+            float newY = Mathf.Sin(Time.time * moveSpeed) * moveRange; // Sin 함수를 사용하여 상하 운동 계산
+            transform.position = new Vector3(originalPosition.x, pivotPoint.position.y + newY, originalPosition.z);
+        }
+        else
+        {
+            float newX = Mathf.Sin(Time.time * moveSpeed) * moveRange; // Sin 함수를 사용하여 좌우 운동 계산
+            transform.position = new Vector3(originalPosition.x + newX, pivotPoint.position.y, originalPosition.z);
+        }
     }
 
 }
