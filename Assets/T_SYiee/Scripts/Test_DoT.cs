2c8c0ee4c1907a91a2a845e10022c9d1b2db8930
SYiee <zyx2009@naver.com>
2024-01-22 19:32:10 +0900

    ADD : DoTween

---
+++ Assets/T_SYiee/Scripts/Test_DoT.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening; //importing
+
+public class Test_DoT : MonoBehaviour
+{
+    //public Text text;
+    //public Ease ease;
+}
