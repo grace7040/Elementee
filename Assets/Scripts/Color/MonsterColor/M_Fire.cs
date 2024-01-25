d5431edb1ac33c5f96150ac70beef1a96a0012eb
Kunho Heo <93777460+Heokunho@users.noreply.github.com>
2024-01-25 22:52:45 +0900

    Fix : Red

--- Assets/Scripts/Color/MonsterColor/M_Fire.cs
+++ Assets/Scripts/Color/MonsterColor/M_Fire.cs
@@ -8,28 +8,28 @@ public class M_Fire : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
-        Transform player = GameObject.FindGameObjectWithTag("Player").transform;
-        GameObject Red = GameObject.Find("M_Red");
+        //SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        //Transform player = GameObject.FindGameObjectWithTag("Player").transform;
+        //GameObject Red = GameObject.Find("M_Red");
 
         Destroy(gameObject, 2.0f);
 
-        if (player != null)
-        {
-            // 몬스터와 플레이어의 위치 차이를 계산합니다.
-            float distance = player.position.x - Red.gameObject.transform.position.x;
+        //if (player != null)
+        //{
+        //    // 몬스터와 플레이어의 위치 차이를 계산합니다.
+        //    float distance = player.position.x - Red.gameObject.transform.position.x;
 
-            // 플레이어가 몬스터의 왼쪽에 있으면 좌우를 뒤집습니다.
-            if (distance < 0f)
-            {
-                sprite.flipX = true;
-            }
-            // 플레이어가 몬스터의 오른쪽에 있으면 좌우를 뒤집지 않습니다.
-            else if (distance > 0f)
-            {
-                sprite.flipX = false;
-            }
-        }
+        //    // 플레이어가 몬스터의 왼쪽에 있으면 좌우를 뒤집습니다.
+        //    if (distance < 0f)
+        //    {
+        //        sprite.flipX = true;
+        //    }
+        //    // 플레이어가 몬스터의 오른쪽에 있으면 좌우를 뒤집지 않습니다.
+        //    else if (distance > 0f)
+        //    {
+        //        sprite.flipX = false;
+        //    }
+        //}
     }
 
     // Update is called once per frame
