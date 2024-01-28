3f683f7d1fe30c6c0e436616c6a8d3a7fe35e9a8
CHERRY <84861196+CHERRY-611@users.noreply.github.com>
2024-01-28 20:52:52 +0900

    Feat: JumpPlatform -> Spring 이미지/애니메이션 적용

--- Assets/Scripts/Obastacles/OB_JumpPlatform.cs
+++ Assets/Scripts/Obastacles/OB_JumpPlatform.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class OB_JumpPlatform : MonoBehaviour
 {
     public float jumpForce = 30f; // 점프 힘 조절용 변수
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // 플레이어와 발판의 충돌을 확인합니다.
         if (collision.gameObject.CompareTag("Player"))
         {
+            anim.Play("spring",-1,0.3f);
             Rigidbody2D playerRigidbody = collision.gameObject.GetComponent<Rigidbody2D>();
 
             // 플레이어 객체에 점프 힘을 적용합니다.
