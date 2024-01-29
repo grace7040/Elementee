d8ec2e44d56530d944df78470c96f7a13a1874a7
grace7040 <81251069+grace7040@users.noreply.github.com>
2024-01-29 16:55:52 +0900

    Feat: 랜덤 아이템 박스 추가

---
+++ Assets/Scripts/Obastacles/RandomBox.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomBox : MonoBehaviour
+{
+    public GameObject[] randomObjects;
+    bool isUsed = false;
+    int randomIdx;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isUsed) return;
+        if (collision.gameObject.tag == "Weapon" || collision.gameObject.tag == "WeaponB")
+        {
+            isUsed = true;
+            randomIdx = Random.Range(0, randomObjects.Length);
+            Instantiate(randomObjects[randomIdx], transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
