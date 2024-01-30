e70064a1afd5f2a10503b01b1e0db074bf63210a
SYiee <zyx2009@naver.com>
2024-01-30 19:38:28 +0900

    Fix : Player Speed Down
    10 -> 7

--- Assets/T_SYiee/Scripts/Test_DoT.cs
+++ Assets/T_SYiee/Scripts/Test_DoT.cs
@@ -8,13 +8,24 @@ public class Test_DoT : MonoBehaviour
     public Color startColor;
     public Color endColor;
     public Renderer renderer;
+    public SpriteRenderer image;
 
     void Start()
     {
         // 색상 값을 보간하는 Tween 작업 생성
-        Tween colorTween = DOTween.To(() => startColor, color => renderer.material.color = color, endColor, 5f);
+        //Tween colorTween = DOTween.To(() => startColor, color => renderer.material.color = color, endColor, 5f);
 
         // Tween 작업 실행
-        colorTween.Play().SetDelay(3f);
+        //colorTween.Play().SetDelay(3f);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            image.DOFade(0.2f, 0.5f).SetLoops(6, LoopType.Yoyo);
+            //image.DOFade(0.5f, 1f);
+            //image.DOColor(endColor, 0.5f).Loops();
+        }
     }
 }
