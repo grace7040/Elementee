b3a88db87916c40bf5ca6bc9e9e73ce5d90ce8f7
grace7040 <81251069+grace7040@users.noreply.github.com>
2024-02-04 00:27:55 +0900

    Feat: Map Objects 프리팹 수정
    ColorDoor: 콜라이더가 너무 안쪽에 작게 있어서 콜라이더 크기 조정
    ColorFountain: ㄷ자 형태의 틀에 대한 콜라이더 삭제
    FallingPlatform: 떨어지는 시간 기본값 조정(3초->0.5초)
    JumpPlatform: local 기준 점프 하도록 수정, but 제대로 되진 않음

--- Assets/Scripts/Obastacles/Platform/OB_JumpPlatform.cs
+++ Assets/Scripts/Obastacles/Platform/OB_JumpPlatform.cs
@@ -6,10 +6,13 @@ public class OB_JumpPlatform : MonoBehaviour
 {
     public float jumpForce = 30f; // 점프 힘 조절용 변수
     private Animator anim;
+    Vector3 jumpVector;
 
     private void Start()
     {
         anim = this.GetComponent<Animator>();
+        jumpVector = transform.up * jumpForce;
+        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -17,14 +20,14 @@ public class OB_JumpPlatform : MonoBehaviour
         // 플레이어와 발판의 충돌을 확인합니다.
         if (collision.gameObject.CompareTag("Player"))
         {
-            
+            Debug.Log(jumpVector);
             Rigidbody2D playerRigidbody = collision.gameObject.GetComponent<Rigidbody2D>();
 
             // 플레이어 객체에 점프 힘을 적용합니다.
             if (playerRigidbody != null)
             {
-                Vector3 jumpVector = Vector3.up * jumpForce;
-                playerRigidbody.AddForce(jumpVector, ForceMode2D.Impulse);
+                collision.rigidbody.AddForce(jumpVector, ForceMode2D.Impulse);
+                //playerRigidbody.AddForce(jumpVector, ForceMode2D.Impulse);
             }
 
             // 애니메이션 재생
