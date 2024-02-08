9d5e5f5fa83b7158cc32f2c6655d37f15ef4af63
CHERRY-611 <84861196+CHERRY-611@users.noreply.github.com>
2024-02-09 03:25:31 +0900

    Feat: FallingPlatform 발판 부러지도록

--- Assets/Scripts/Obastacles/Platform/FallingPlatform.cs
+++ Assets/Scripts/Obastacles/Platform/FallingPlatform.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class FallingPlatform : MonoBehaviour
 {
+    public List<GameObject> Platforms = new List<GameObject>();
     public float fallDelay = 0f;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         
         if(collision.gameObject.tag == "Player")
         {
-            this.CallOnDelay(fallDelay, ()=>{ gameObject.transform.parent.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None; });
+            GetComponent<BoxCollider2D>().enabled = false;
+            this.CallOnDelay(fallDelay, ()=>{ Platforms[0].transform.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None; });
+            this.CallOnDelay(fallDelay, ()=>{ Platforms[1].transform.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None; });
         }
     }
 }
