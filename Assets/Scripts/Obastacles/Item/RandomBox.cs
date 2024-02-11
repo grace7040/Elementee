78a88912cffdf20fb27dbf433f599cfbbf315057
grace7040 <81251069+grace7040@users.noreply.github.com>
2024-02-11 22:03:59 +0900

    Feat: map 5 7 8

--- Assets/Scripts/Obastacles/Item/RandomBox.cs
+++ Assets/Scripts/Obastacles/Item/RandomBox.cs
@@ -18,14 +18,17 @@ public class RandomBox : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (isUsed) return;
-        if (collision.gameObject.CompareTag("Weapon") || collision.gameObject.CompareTag("WeaponB"))
+        if (collision.gameObject.CompareTag("Weapon") || collision.gameObject.CompareTag("WeaponB") || collision.gameObject.CompareTag("WeaponThrow")) 
         {
             isUsed = true;
             randomIdx = Random.Range(0, randomObjects.Length);
 
             anim.Play("RandomBox", -1, 0f);
 
-            this.CallOnDelay(2f, () => { Destroy(gameObject); Instantiate(randomObjects[randomIdx], transform.position, Quaternion.identity);});
+            this.CallOnDelay(1.5f, () => { 
+                Instantiate(randomObjects[randomIdx], transform.position, Quaternion.identity, this.transform.parent);
+                Destroy(gameObject);
+            });
         }
     }
 }
