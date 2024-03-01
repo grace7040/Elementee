8676633c907608eb6444e5a25a8ee17c09667d7b
SYiee <zyx2009@naver.com>
2024-03-02 02:18:50 +0900

    Feat : 몬스터 피격시 닳는 피 fade out & POP UP

--- Assets/Scripts/Monster/M_DamageText.cs
+++ Assets/Scripts/Monster/M_DamageText.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using TMPro;
 
 public class M_DamageText : PoolAble
 {
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(this.gameObject, 0.5f);
+        transform.DOLocalMoveY(13, 1.3f);
+        this.gameObject.GetComponent<TextMeshPro>().DOFade(0,2f);
+        Destroy(this.gameObject, 2f);
     }
 
     // Update is called once per frame
