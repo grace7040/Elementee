544f0e0b7e41ee9bf91ed9fb2d99e8108f14db33
DESKTOP-SEEUN\tpdms <grace7040@naver.com>
2024-03-09 11:43:44 +0900

    Feat: Add Ball

---
+++ Assets/Scripts/Obastacles/GoalPost.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalPost : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    FrictionJoint2D  fj;
+    public GameObject star;
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ball")){
+            fj = collision.gameObject.AddComponent<FrictionJoint2D>();
+            fj.connectedBody = rigid;
+            fj.autoConfigureConnectedAnchor = false;
+            fj.connectedAnchor = new Vector2(0, 0);
+            fj.maxForce = 100;
+            fj.maxTorque = 5;
+            collision.GetComponent<CapsuleCollider2D>().isTrigger = true;
+            star.SetActive(true);
+        }
+    }
+}
