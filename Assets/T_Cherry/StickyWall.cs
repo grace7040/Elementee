84e089f53ff61a06ace5af73e3ac4a0f51223b82
CHERRY <84861196+CHERRY-611@users.noreply.github.com>
2024-03-10 17:39:48 +0900

    Feat: Cart Sprite 바퀴 분리

---
+++ Assets/T_Cherry/StickyWall.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyWall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public bool WallSliding;
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.CompareTag("Player"))
+    //    {
+    //        collision.GetComponent<PlayerController>().Color.WallSliding = true;
+
+    //    }
+    //}
+}
