93b416eec73eaaa5cfcbd905594de368875364f7
DESKTOP-SEEUN\tpdms <grace7040@naver.com>
2024-03-12 23:37:05 +0900

    Feat: BtnDoor 의 버튼이 무게로 작동하도록 수정

--- Assets/T_SYiee/Scripts/BtnPushDoor.cs
+++ Assets/T_SYiee/Scripts/BtnPushDoor.cs
@@ -7,37 +7,30 @@ public class BtnPushDoor : MonoBehaviour
 {
     public Transform Door;
     public Transform DoorOpen;
-    public Transform BtnClicked;
 
     private Vector3 OriginalDoor;
-    private Vector3 OriginalBtn;
 
     private bool isPush = false;
 
     private void Start()
     {
-        OriginalBtn = gameObject.transform.position;
         OriginalDoor = Door.transform.position;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player") && isPush == false)
+        if (collision.gameObject.CompareTag("Btn") && isPush == false)
         {
             isPush = true;
-            gameObject.transform.DOMove(BtnClicked.position, 0.2f);
             Door.DOMove(DoorOpen.position, 2f);
-
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.CompareTag("Btn"))
         {
-            gameObject.transform.DOMove(OriginalBtn, 0.2f);
             Door.DOMove(OriginalDoor, 2f);
             isPush = false;
-
         }
     }
 
