f4d59ace8465be610254a786918e6c23b85646de
Kunho Heo <93777460+Heokunho@users.noreply.github.com>
2024-04-02 23:55:26 +0900

    Feat : Coin sound

--- Assets/T_Cherry/Coin.cs
+++ Assets/T_Cherry/Coin.cs
@@ -10,6 +10,7 @@ public class Coin : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             GameManager.Instance.mapCoin++;
+            AudioManager.Instacne.PlaySFX("Star");
             Destroy(this.gameObject);
         }
     }
