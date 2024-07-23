a0d398d2e012dbec7a6f6fcd4557790bdca20a20
CHERRY-611 <84861196+CHERRY-611@users.noreply.github.com>
2024-07-23 22:11:01 +0900

    Feat: MapObj Sprite 변경

---
+++ Assets/T_Cherry/OB_ChangeScale.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OB_ChangeScale : MonoBehaviour
+{
+    GameObject _body;
+    bool _decrease = true;
+    float _decreseSpeed;
+
+
+    public int Speed = 8;
+    public float minX = 0;
+    public float maxX = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _body = this.gameObject;
+        _decreseSpeed = Speed * 0.001f;
+
+        Debug.Log(_body.GetComponent<Transform>().localScale.x);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (_decrease)
+        {
+            _body.transform.localScale = new Vector3(_body.transform.localScale.x - _decreseSpeed
+                                                , _body.transform.localScale.y
+                                                , _body.transform.localScale.z);
+        }
+        else
+        {
+            _body.transform.localScale = new Vector3(_body.transform.localScale.x + _decreseSpeed
+                                                    , _body.transform.localScale.y
+                                                    , _body.transform.localScale.z);
+        }
+
+        if (_body.GetComponent<Transform>().localScale.x > maxX) _decrease = true;
+
+        if (_body.GetComponent<Transform>().localScale.x < minX) _decrease = false;
+    }
+}
