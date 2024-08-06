07dbcaddbaaf754237d53ee25772be85d1b28b24
DESKTOP-SEEUN\tpdms <grace7040@naver.com>
2024-08-07 02:49:32 +0900

    Fix: Falling Platform 아래에서 닿아도 떨어지는 오류 수정

--- Assets/Scripts/Obastacles/Platform/FallingPlatform.cs
+++ Assets/Scripts/Obastacles/Platform/FallingPlatform.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class FallingPlatform : MonoBehaviour
 {
-    public List<GameObject> Platforms = new List<GameObject>(2);
+    [SerializeField] List<GameObject> Platforms = new List<GameObject>(2);
+    [SerializeField] BoxCollider2D _bottomCollider;
     public float FallDelay = 0f;
 
-    BoxCollider2D _collider;
+    BoxCollider2D _topCollider;
     Rigidbody2D _leftRigid;
     Rigidbody2D _rightRigid;
     BoxCollider2D _leftCollider;
     BoxCollider2D _rightCollider;
     private void Start()
     {
-        _collider = GetComponent<BoxCollider2D>();
+        _topCollider = GetComponent<BoxCollider2D>();
         _leftRigid = Platforms[0].transform.GetComponent<Rigidbody2D>();
         _rightRigid = Platforms[1].transform.GetComponent<Rigidbody2D>();
         _leftCollider = Platforms[0].transform.GetComponent<BoxCollider2D>();
@@ -22,7 +23,8 @@ public class FallingPlatform : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        _collider.enabled = false;
+        _topCollider.enabled = false;
+        _bottomCollider.enabled = false;
         _leftCollider.enabled = true;
         _rightCollider.enabled = true;
         this.CallOnDelay(FallDelay, () => { _leftRigid.constraints = RigidbodyConstraints2D.None; });
